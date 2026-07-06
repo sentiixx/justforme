8bfbbb11c982ec11b7a2673d03b48bbbf7c95e86
sentiixx <sentiixx@gmail.com>
2026-07-06 19:16:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/pixel', async (req, res) => {
       .resize(targetSize, targetSize, { fit: 'fill' })
       .png({ 
         palette: true, 
-        colors: 192,       // Zwingt Sharp auf exakt 48 Farben
+        colors: 96,       // Zwingt Sharp auf exakt 48 Farben
         dither: 1.0       // Aktiviert perfektes Dithering, damit Farbverläufe nicht matschig/grau werden
       })
       .toBuffer();
